c7fb6776c8643183cb2e0a53b685f93bc30df628
Emily Dalton <emilyedalton@gmail.com>
2019-06-19 09:25:34 -0500

    popup size

--- src/components/Markers.js
+++ src/components/Markers.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Popup, Icon, Image} from 'semantic-ui-react'
+import { Grid, Popup, Icon, Image} from 'semantic-ui-react'
 
 class Markers extends Component {
     render() {
@@ -7,14 +7,26 @@ class Markers extends Component {
         return (
             <div>
 <Popup
+size="huge"
+
 trigger=
 { <Icon name='marker' size ='big' color ='red'/>  }
 >
 <Popup.Header>{this.props.name}</Popup.Header>
 <Popup.Content>
-<Image src={this.props.image} />
 
-{this.props.description}</Popup.Content>
+<Grid style ={{border:"2px solid green"}} columns={2}>
+<Grid.Column width={16} style ={{border:"2px solid red"}}>
+<Image src={this.props.image}/>
+</Grid.Column>
+<Grid.Row/>
+<Grid.Column width={16} style ={{border:"2px solid blue"}}>
+{this.props.description}
+</Grid.Column>
+
+</Grid>
+</Popup.Content>
+
 </Popup>
 </div>
         )
